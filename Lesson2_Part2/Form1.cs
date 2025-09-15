097dd7b8957d0efa874676f55cb8638d0ceb3542
elaizabautista <2023-2-02500@lpunetwork.edu.ph>
2025-09-16 00:28:55 +0800

    final

--- Lesson2_Part2/Form1.cs
+++ Lesson2_Part2/Form1.cs
@@ -30,6 +30,12 @@ namespace DSAL01E_Lesson2_Activity_Bautista
         }
         private void Form1_Load(object sender, EventArgs e)
         {
+            creditunits1.Enabled = false;
+            total1.Enabled = false;
+            totaltuition1.Enabled = false;
+            totalmis1.Enabled = false;
+            tuitionandfee1.Enabled = false;
+
             comboBox1.Items.Add("Bachelor of Science in Computer Science");
             comboBox1.Items.Add("Bachelor of Science in Aeronautical Engineering");
             comboBox1.Items.Add("Bachelor of Science in Civil Engineering");
@@ -82,6 +88,46 @@ namespace DSAL01E_Lesson2_Activity_Bautista
             bookletlistbox.Items.Add(booklet1.Text);
             totalnumunitslistbox.Items.Add(total1.Text);
             totalfeeslistbox.Items.Add(tuitionandfee1.Text);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Calculate Credit Units for this entry
+            double creditUnits = Convert.ToDouble(unitlecture1.Text) + Convert.ToDouble(unitlaboratory1.Text);
+            creditunits1.Text = creditUnits.ToString();
+            // Add the current entry's credit units to the running total
+            double previousTotal = 0;
+
+            // Check if there’s already a total
+            if (total1.Text != "")
+            {
+                previousTotal = Convert.ToDouble(total1.Text);
+            }
+
+            // Add the new credit units
+            double totalUnits = previousTotal + creditUnits;
+
+            // Show the updated total
+            total1.Text = totalUnits.ToString();
+
+            // Calculate Total Tuition Fee
+            const double TUITION_FEE_PER_UNIT = 1700.00;
+            double totalTuitionFee = totalUnits * TUITION_FEE_PER_UNIT;
+            totaltuition1.Text = totalTuitionFee.ToString("n");
+
+            // Get miscellaneous fee values
+            double labFee = Convert.ToDouble(labfee1.Text);
+            double ciscoFee = Convert.ToDouble(cisco1.Text);
+            double bookletFee = Convert.ToDouble(booklet1.Text);
+
+            // Calculate Total Miscellaneous Fee
+            double totalMiscFee = labFee + ciscoFee + bookletFee;
+            totalmis1.Text = totalMiscFee.ToString("n");
+
+            // Calculate Total Tuition and Fees
+            double totalTuitionAndFees = totalTuitionFee + totalMiscFee;
+            tuitionandfee1.Text = totalTuitionAndFees.ToString("n");
 
         }
     }
